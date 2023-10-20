588eb28a3c1a099507276198e09ec3e7f31c39d0
Jeanherline Santiago <118523315+jeanherline@users.noreply.github.com>
2023-10-20 09:05:20 +0800

    Add files via upload

---
+++ Wind_Forms/Signup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Wind_Forms
+{
+    public partial class Signup : Form
+    {
+        db data = new db();
+        public Signup()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+        private void Signup_Activated(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0) {
+                textBox3.Text = listView1.SelectedItems[0].Text;
+                textBox1.Text = listView1.SelectedItems[0].SubItems[1].Text;
+                textBox2.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            }
+        }
+
+        private void Signup_Load(object sender, EventArgs e)
+        {
+            data.load_data("select * from tblReg", listView1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string qryCreate = "insert into tblReg values('" + textBox1.Text + "', '" + textBox2.Text + "')";
+            string qryUpdate = "update tblReg Set email = '" + textBox1.Text + "', password = '" + textBox2.Text + "' where id = '" + textBox3.Text + "'";
+            data.saveData(qryCreate, qryUpdate, textBox3);
+            data.load_data("select * from tblReg", listView1);
+
+            textBox3.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox4.Text != "")
+            {
+                data.load_data("select * from tblReg where id = '" + textBox4.Text + "'", listView1);
+            } else
+            {
+                data.load_data("select * from tblReg", listView1);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            data.delete("DELETE FROM tblReg where id = '" + textBox3.Text + "'", textBox3);
+            data.load_data("select * from tblReg", listView1);
+            textBox3.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
---
+++ Wind_Forms/db.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace Wind_Forms
+{
+    class db
+    {
+        public SqlConnection conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Jea;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+        public void load_data(string q, ListView lv)
+        {
+            lv.Items.Clear();
+            try
+            {
+                SqlDataReader dr;
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = q;
+                conn.Open();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        ListViewItem item = new ListViewItem(dr["Id"].ToString());
+                        item.SubItems.Add(dr["email"].ToString());
+                        item.SubItems.Add(dr["password"].ToString());
+                        lv.Items.Add(item);
+                    }
+                }
+                dr.Close();
+                conn.Close();
+            }
+            catch(Exception e)
+            {
+                //cn.Close();
+                MessageBox.Show(e.Message.ToString());
+            }
+         
+        }
+
+        public void saveData(string sqlCreate, string sqlUpdate, TextBox txtid) {
+            cmd.Connection = conn;
+
+            SqlCommand cmdCheckId = new SqlCommand("select id from tblReg where id = '" + txtid.Text + "'", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmdCheckId);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                //update
+                conn.Open();
+                SqlCommand cmd1 = conn.CreateCommand();
+                cmd1.CommandType = CommandType.Text;
+                cmd1.CommandText = sqlUpdate;
+
+                cmd1.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Record Updated Successfully!");
+
+            }
+            else {
+                //Insert
+                conn.Open();
+                SqlCommand cmd2 = conn.CreateCommand();
+                cmd2.CommandType = CommandType.Text;
+                cmd2.CommandText = sqlCreate;
+
+                cmd2.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Record Added Successfully!");
+            }
+            conn.Close();
+        }
+
+        public void delete(string sqlDelete, TextBox txtid)
+        {
+            cmd.Connection = conn;
+
+            SqlCommand cmdCheckId = new SqlCommand("select id from tblReg where id = '" + txtid.Text + "'", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmdCheckId);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                conn.Open();
+                SqlCommand cmd1 = conn.CreateCommand();
+                cmd1.CommandType = CommandType.Text;
+                cmd1.CommandText = sqlDelete;
+
+                cmd1.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Record Deleted Successfully!");
+
+            }
+            else
+            {
+                MessageBox.Show("Record Doesn't Exist!");
+            }
+            conn.Close();
+        }
+    }
+}
